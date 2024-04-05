38922b764e5f767baa31ab463201ee910e1163b0
OOHehir <40183643+OOHehir@users.noreply.github.com>
2024-04-05 01:44:26 +0100

    Added touch functionality

--- src/main.c
+++ src/main.c
@@ -11,42 +11,72 @@
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
-
 /*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
+ * Get configuration from the devicetree
  */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+#define LED0_NODE DT_ALIAS(led0)
+#if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#error "Unsupported board: led0 devicetree alias is not defined"
+#endif
 
-// STATIC_ASSERT(led.port != DT_INVALID_NODE, "Unsupported board.");
+#define TOUCH0_NODE DT_ALIAS(touch0)
+
+#if !DT_NODE_HAS_STATUS(TOUCH0_NODE, okay)
+#error "Unsupported board: touch0 devicetree alias is not defined"
+#endif
+
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios, {0});
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct  gpio_dt_spec touch0 =  GPIO_DT_SPEC_GET_OR(LED0_NODE, gpios, {0});
+static struct gpio_callback touch_cb_data;
+
+void touch_activated(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+    printf("Touch activated at %" PRIu32 "\n", k_cycle_get_32());
+    gpio_pin_toggle_dt(&led);
+}
 
 int main(void)
 {
-	int ret;
-	bool led_state = true;
+    int ret;
 
-	if (!gpio_is_ready_dt(&led)) {
-		return 0;
-	}
+    if (!gpio_is_ready_dt(&led)) {
+        return 0;
+    }
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return 0;
-	}
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        return 0;
+    }
 
-    printf("Hello World! %s\n", CONFIG_BOARD);
+    if (!gpio_is_ready_dt(&touch0)) {
+        printf("Error: touch device %s is not ready\n",
+               touch0.port->name);
+        return 0;
+    }
 
-	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
-		}
+    ret = gpio_pin_configure_dt(&touch0, GPIO_INPUT);
+    if (ret < 0) {
+        printf("Error %d: failed to configure %s pin %d\n",
+               ret, touch0.port->name, touch0.pin);
+        return 0;
+    }
 
-		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
-		k_msleep(SLEEP_TIME_MS);
-	}
-	return 0;
+    ret = gpio_pin_interrupt_configure_dt(&touch0, GPIO_INT_EDGE_TO_ACTIVE);
+        if (ret != 0) {
+        printf("Error %d: failed to configure interrupt on %s pin %d\n",
+            ret, touch0.port->name, touch0.pin);
+        return 0;
+    }
+
+    gpio_init_callback(&touch_cb_data, touch_activated, BIT(touch0.pin));
+    gpio_add_callback(touch0.port, &touch_cb_data);
+    printf("Set up button at %s pin %d\n", touch0.port->name, touch0.pin);
+
+    printf("Board: %s\n", CONFIG_BOARD);
+
+    while (1) {
+        k_msleep(SLEEP_TIME_MS);
+    }
+    return 0;
 }
